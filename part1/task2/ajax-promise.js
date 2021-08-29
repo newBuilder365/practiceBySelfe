14c6e0836932ce165e7df3b47b027721d3b90fb9
newBuilder365 <772512900@qq.com>
2021-08-29 20:11:03 +0800

    手写ajax

---
+++ part1/task2/ajax-promise.js
@@ -0,0 +1,21 @@
+function ajax(url) {
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', url)
+        xhr.responseType = 'json'
+        xhr.onload = function () {
+            if (this.status === 200) {
+                resolve(this.response)
+            } else {
+                reject(new Error(this.statusText))
+            }
+        }
+        xhr.send()
+    })
+}
+
+ajax('/api/user.json').then(function (res) {
+    console.log(res)
+}, function (error) {
+    console.log(error)
+})
